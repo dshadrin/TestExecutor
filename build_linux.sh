258f10e2cae6cae4c4c8c1d232c42f0212e2e027
Dmitriy Shadrin <dshadrin@gmail.com>
2022-07-05 20:57:27 +1000

    fix linux build

--- build_linux.sh
+++ build_linux.sh
@@ -2,7 +2,7 @@
 rm -fr build
 mkdir build
 cd build
-/home/dima/opt/Qt/Tools/CMake/bin/cmake .. -G"Unix Makefiles" -DCMAKE_BUILD_TYPE:STRING="Debug" -DUNIX=ON
+cmake .. -G"Unix Makefiles" -DCMAKE_BUILD_TYPE:STRING="Debug" -DUNIX=ON
 pause
-/home/dima/opt/Qt/Tools/CMake/bin/cmake --build ./ -j 3 --target install
+cmake --build ./ -j 3 --target install
 
